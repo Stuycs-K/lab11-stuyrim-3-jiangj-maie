1d083a2b53f3f63d1bd0110c19f71afdb7937011
JerryJiang531 <jerryj36@nycstudents.net>
2025-01-10 09:56:52 -0500

    added field transformed, implemented attack

--- Boss.java
+++ Boss.java
@@ -1,14 +1,20 @@
 public class Boss extends Adventurer{
   private int special;
   private int specialMax = 20;
+  private boolean transformed;
 
   public Boss(String name){
     super(name);
     setSpecial(0);
+    setTransformed(false);
   }
   public Boss(String name, int hp){
     super(name,hp);
     setSpecial(0);
+    setTransformed(false);
+  }
+  public void setTransformed(boolean n){
+    transformed = n;
   }
   public String getSpecialName(){
     return "Fury";
@@ -23,10 +29,22 @@ public class Boss extends Adventurer{
   public int getSpecialMax(){
     return specialMax;
   }
+  public boolean getTransformed(){
+    return transformed;
+  }
   public String attack(Adventurer other){
-    other.applyDamage(5);
-    setSpecial(getSpecial() - 1);
-    return "Attacked " + other + " for 5 damage!\n" + getSpecialName() + " reduced by 1";
+    if (getTransformed()){
+      other.applyDamage(6);
+      setHp(getHP() + 3);
+      setSpecial(getSpecial() + 2);
+      return "Bit " + other + " for 6 damage!\n" + getSpecialName() + " increased by 2\n HP increased by 3";
+    }else{
+      other.applyDamage(4);
+      setHP(getHP() + 1);
+      setSpecial(getSpecial() + 1);
+      return "Bit " + other + " for 4 damage!\n" + getSpecialName() + " increased by 1\n HP increased by 1";
+    }
+
   }
 
   //heall or buff the target adventurer
