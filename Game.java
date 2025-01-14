7381adcda957783f4e13188c2725585aa231bfbd
JerryJiang531 <jerryj36@nycstudents.net>
2025-01-14 09:51:13 -0500

    worked on creating a party

--- Game.java
+++ Game.java
@@ -126,7 +126,8 @@ public class Game{
     Text.hideCursor();
     Text.clear();
 
-
+    Scanner in = new Scanner(System.in);
+    String input = "";
     //Things to attack:
     //Make an ArrayList of Adventurers and add 1-3 enemies to it.
     //If only 1 enemy is added it should be the boss class.
@@ -139,6 +140,24 @@ public class Game{
     //Adventurers you control:
     //Make an ArrayList of Adventurers and add 2-4 Adventurers to it.
     ArrayList<Adventurer> party = new ArrayList<>();
+    for (int i = 0; i < 3; i++){ // loop to add 3 allies
+      String partyPreprompt = "Draft a player to add to your party: (A)rcher, (N)inja, (C)odeWarrior, (R)andom";
+      input = userInput(in);
+      if(input.equals("Archer") || input.equals("A")){
+        party.add(new Archer());
+      }
+      else if(input.equals("Ninja") || input.equals("N")){
+        party.add(new Ninja());
+      }
+      else if(input.equals("CodeWarrior") || input.equals("C")){
+        party.add(new Archer());
+      }
+      else if(input.equals("Random") || input.equals("R")){
+
+      }
+
+
+    }
     /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
     //YOUR CODE HERE
     /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
@@ -147,8 +166,8 @@ public class Game{
     int whichPlayer = 0;
     int whichOpponent = 0;
     int turn = 0;
-    String input = "";//blank to get into the main loop.
-    Scanner in = new Scanner(System.in);
+    input = "";//blank to get into the main loop.
+
     //Draw the window border
 
     //You can add parameters to draw screen!
